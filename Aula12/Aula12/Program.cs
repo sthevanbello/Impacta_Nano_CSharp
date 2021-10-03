4ed5185951fb82d06b708a2e639d52dc94fe6e69
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-03 16:51:31 -0300

    StreamWriter

---
+++ Aula12/Aula12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Aula12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var meusDocumentos = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string path = $@"{meusDocumentos}\teste.txt";
+
+            using (var file = new StreamWriter(path, true))
+            {
+                file.WriteLine("Teste do Nano Degree");
+                Console.WriteLine("Ok");
+            }
+        }
+    }
+}
