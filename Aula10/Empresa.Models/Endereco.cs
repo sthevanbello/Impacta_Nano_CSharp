f6ea68d6dfc2d5cf0882edfed0b716d17278e7e2
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-02 22:12:45 -0300

    Classe Endereco adicionada

---
+++ Aula10/Empresa.Models/Endereco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa.Models
+{
+    class Endereco
+    {
+        public int Id { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Municipio { get; set; }
+        public string UF { get; set; }
+        public int Numero { get; set; }
+
+    }
+}
